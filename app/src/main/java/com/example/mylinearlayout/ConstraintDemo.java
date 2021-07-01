f40cf1f5992d6e6ad727197d6542e1fea05fedba
pooja-gif <poojagits5@gmail.com>
2021-07-01 20:36:57 +0530

    committed

---
+++ app/src/main/java/com/example/mylinearlayout/ConstraintDemo.java
@@ -0,0 +1,15 @@
+package com.example.mylinearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_demo);
+        getSupportActionBar().hide();
+    }
+}
